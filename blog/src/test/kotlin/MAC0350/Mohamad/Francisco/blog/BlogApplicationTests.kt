2dd87357b16d2193f37f61f37824cc59c72c8558
Francisco de Castro Leal Henriques <franciscolealhenriques@usp.br>
2024-04-09 12:42:51 -0300

    Add Spring Boot project
    The project has changed entirely, and will consist of a "social network"
    to share videos of a specific game and comment on them, so, we will be
    using Spring Boot with kotlin to make it.

---
+++ blog/src/test/kotlin/MAC0350/Mohamad/Francisco/blog/BlogApplicationTests.kt
@@ -0,0 +1,13 @@
+package MAC0350.Mohamad.Francisco.blog
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
