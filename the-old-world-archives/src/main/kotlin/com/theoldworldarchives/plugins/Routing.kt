1a93f734c34c905b6bca496420d449e988a16a13
Francisco de Castro Leal Henriques <franciscolealhenriques@usp.br>
2024-05-12 21:36:21 -0300

    Add redirection from / to mainpage.html

--- the-old-world-archives/src/main/kotlin/com/theoldworldarchives/plugins/Routing.kt
+++ the-old-world-archives/src/main/kotlin/com/theoldworldarchives/plugins/Routing.kt
@@ -3,11 +3,15 @@ package com.theoldworldarchives.plugins
 import com.theoldworldarchives.routes.postRouting
 import io.ktor.server.application.*
 import io.ktor.server.http.content.*
+import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
     routing {
         postRouting()
         staticResources("/static", "files")
+        get("/") {
+            call.respondRedirect("/static/mainpage.html")
+        }
     }
 }
