84ea1f5c81db75a7427be30b87b7cfa162079b61
Francisco de Castro Leal Henriques <franciscolealhenriques@usp.br>
2024-05-12 21:27:30 -0300

    Add .html file as main page
    A main page with a brief information about the project, as well as a video, was added
    for testing purposes, but serves as a way to show a bit of our project, and stablish a
    foundation for us to make other static content pages, such as the wiki and "about us" page.

--- the-old-world-archives/build.gradle.kts
+++ the-old-world-archives/build.gradle.kts
@@ -27,6 +27,7 @@ dependencies {
     implementation("io.ktor:ktor-server-content-negotiation-jvm")
     implementation("io.ktor:ktor-server-core-jvm")
     implementation("io.ktor:ktor-serialization-kotlinx-json-jvm")
+    implementation("io.ktor:ktor-server-freemarker-jvm")
     implementation("io.ktor:ktor-server-netty-jvm")
     implementation("io.ktor:ktor-client-json:1.6.6")
     implementation("ch.qos.logback:logback-classic:$logback_version")
--- the-old-world-archives/src/main/kotlin/com/theoldworldarchives/plugins/Routing.kt
+++ the-old-world-archives/src/main/kotlin/com/theoldworldarchives/plugins/Routing.kt
@@ -2,14 +2,12 @@ package com.theoldworldarchives.plugins
 
 import com.theoldworldarchives.routes.postRouting
 import io.ktor.server.application.*
-import io.ktor.server.response.*
+import io.ktor.server.http.content.*
 import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
     routing {
-        get("/") {
-            call.respondText("Hello World!")
-        }
         postRouting()
+        staticResources("/static", "files")
     }
 }
