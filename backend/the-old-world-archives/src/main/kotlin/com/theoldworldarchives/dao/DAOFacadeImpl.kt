260e3321558b5f7e4d13c940dc7d957155817c8b
Francisco Henriques <franciscolealhenriques@usp.br>
2024-06-14 13:24:26 -0300

    Add User database functions
    Database functions for adding a user, user to row, deleting user, that sort of thing.

--- backend/the-old-world-archives/src/main/kotlin/com/theoldworldarchives/dao/DAOFacadeImpl.kt
+++ backend/the-old-world-archives/src/main/kotlin/com/theoldworldarchives/dao/DAOFacadeImpl.kt
@@ -7,11 +7,13 @@ import org.jetbrains.exposed.sql.*
 import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
 
 class DAOFacadeImpl : DAOFacade {
+    // Post class
     private fun resultRowToPost(row: ResultRow) = Post(
         id = row[Posts.id],
         title = row[Posts.title],
         videoFilepath = row[Posts.videoFilepath],
-        description = row[Posts.description]
+        description = row[Posts.description],
+        userName = row[Posts.userName]
     )
 
     override suspend fun allPosts(): List<Post> = dbQuery {
@@ -25,25 +27,63 @@ class DAOFacadeImpl : DAOFacade {
             .singleOrNull()
     }
 
-    override suspend fun addNewPost(title: String, videoFilepath: String, description: String): Post? = dbQuery {
+    override suspend fun addNewPost(title: String, videoFilepath: String, description: String, userName: String): Post? = dbQuery {
         val insertStatement = Posts.insert {
             it[Posts.title] = title
             it[Posts.videoFilepath] = videoFilepath
             it[Posts.description] = description
+            it[Posts.userName] = userName
         }
         insertStatement.resultedValues?.singleOrNull()?.let(::resultRowToPost)
     }
 
-    override suspend fun editPost(id: Int, title: String, videoFilepath: String, description: String): Boolean = dbQuery {
+    override suspend fun editPost(id: Int, title: String, videoFilepath: String, description: String, userName: String): Boolean = dbQuery {
         Posts.update({ Posts.id eq id }) {
             it[Posts.title] = title
             it[Posts.videoFilepath] = videoFilepath
             it[Posts.description] = description
+            it[Posts.userName] = userName
         } > 0
     }
 
     override suspend fun deletePost(id: Int): Boolean = dbQuery {
         Posts.deleteWhere { Posts.id eq id } > 0
+    }
+
+    // User class
+    private fun resultRowToUser(row: ResultRow) = User (
+        name = row[Users.name],
+        password = row[Users.password]
+    )
+
+    override suspend fun allUsers(): List<User> = dbQuery {
+        Users.selectAll().map(::resultRowToUser)
+    }
+
+    override suspend fun user(name: String): User? = dbQuery {
+        Users
+            .select { Users.name eq name }
+            .map(::resultRowToUser)
+            .singleOrNull()
+    }
+
+    override suspend fun addNewUser(name: String, password: String): User? = dbQuery {
+        val insertStatement = Users.insert {
+            it[Users.name] = name
+            it[Users.password] = password
+        }
+        insertStatement.resultedValues?.singleOrNull()?.let(::resultRowToUser)
+    }
+
+    override suspend fun editUser(name: String, password: String): Boolean = dbQuery {
+        Users.update({ Users.name eq name }) {
+            it[Users.name] = name
+            it[Users.password] = password
+        } > 0
+    }
+
+    override suspend fun deleteUser(name: String): Boolean = dbQuery {
+        Posts.deleteWhere { Users.name eq name } > 0
     }
 }
 
