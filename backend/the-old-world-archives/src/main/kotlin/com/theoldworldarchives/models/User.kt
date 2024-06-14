47435b08a2cb31ee87bf5c8464106b6b4c9cc67a
Francisco Henriques <franciscolealhenriques@usp.br>
2024-06-14 13:20:45 -0300

    Add User class and table

---
+++ backend/the-old-world-archives/src/main/kotlin/com/theoldworldarchives/models/User.kt
@@ -0,0 +1,23 @@
+package com.theoldworldarchives.models
+
+import kotlinx.serialization.Serializable
+import java.util.concurrent.atomic.AtomicInteger
+import org.jetbrains.exposed.sql.*
+
+@Serializable
+data class User(val name: String, val password: String) {
+    companion object {
+        fun new(name: String, password: String): User {
+            return User(name, password)
+        }
+    }
+}
+
+object Users : Table() {
+    val name = varchar("name", 128)
+    val password = varchar("password", 128)
+
+    override val primaryKey = PrimaryKey(this.name)
+}
+
+
