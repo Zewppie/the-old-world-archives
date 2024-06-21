b4f158d920b9772e7bcf6e3705a7e86cd6efdcd4
Zewppie <abedwalid24@gmail.com>
2024-06-21 18:12:31 -0300

    Added new logo and styles in index.css

--- frontend/src/pages/entities.tsx
+++ frontend/src/pages/entities.tsx
@@ -1,18 +1,20 @@
-import React from 'react';
+import { MantineProvider } from '@mantine/core';
 
 function Entities() {
     return (
-        <div style={{ textAlign: 'center' }}>
-            <h1>This is the entity wiki!</h1>
-            {/*aqui há conteúdo estático*/}
-            <div>
-                <h2> #1 - Puffo </h2>
-                <p>Puffo is a very important entity.</p>
-                {/* aqui mostro o entity_puffo.png como teste*/}
-                <img src="entity_puffo.png" alt="This is Puffo" />
+        <MantineProvider defaultColorScheme="dark">
+            <div style={{ textAlign: 'center' }}>
+                <h1>This is the entity wiki!</h1>
+                {/*aqui há conteúdo estático*/}
+                <div>
+                    <h2> #1 - Puffo </h2>
+                    <p>Puffo is a very important entity.</p>
+                    {/* aqui mostro o entity_puffo.png como teste*/}
+                    <img src="entity_puffo.png" alt="This is Puffo" />
+                </div>
+                {/*aqui eu vou fecho a requisição para o backend*/}
             </div>
-            {/*aqui eu vou fecho a requisição para o backend*/}
-        </div>
+        </MantineProvider>
     );
 };
 export default Entities;
