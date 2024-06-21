bf95ce5bf953bdedb3b64dbbc8548568c085c706
Zewppie <abedwalid24@gmail.com>
2024-06-21 18:12:31 -0300

    Added new logo and styles in index.css

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -1,23 +1,25 @@
 import { Button } from '@mantine/core';
+import { MantineProvider } from '@mantine/core';
 import { Link } from 'react-router-dom';
+import '../index.css'; // Import the index.css file
 
 function Home() {
     return (
-        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-            <div>
-                <h1>Welcome to My Website!</h1>
-                <Link to="/posts">
-                    <Button variant="filled" color="indigo">Go to Posts Page</Button>
-                </Link>
+        <MantineProvider defaultColorScheme="dark">
+            <div className="container">
+                <div className="header">
+                    <img src="the_old_world_archives_logo.png" alt="Logo" className="logo"  />
+                </div>
+                <div className="center">
+                    <Link to="/posts">
+                        <Button variant="filled" color="indigo" style={{ marginRight: '10px' }}>Go to Posts Page</Button>
+                    </Link>
+                    <Link to="/entities">
+                        <Button variant="filled" color="indigo">Go to Entities Page</Button>
+                    </Link>
+                </div>
             </div>
-            {/* outra div pra página da wiki entidades */}
-            <div>
-                <h1>Entity Wiki Page</h1>
-                <Link to="/entities">
-                    <Button variant="filled" color="indigo">Go to Entities Page</Button>
-                </Link>
-            </div>
-        </div>
+        </MantineProvider>
     );
-};
+}
 export default Home;
--- frontend/src/pages/posts.tsx
+++ frontend/src/pages/posts.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import { Button } from '@mantine/core';
+import { MantineProvider } from '@mantine/core';
 
 function Posts() {
     return (
-        <div style={{ textAlign: 'center' }}>
-            <h1>This is the post page!</h1>
-            {/*aqui eu vou fazer uma requisição para o backend*/}
-            <Button variant="filled" color="indigo">Get a web post</Button>
-            {/*aqui eu vou fecho a requisição para o backend*/}
-        </div>
+        <MantineProvider defaultColorScheme="dark">
+                <div style={{ textAlign: 'center' }}>
+                    <h1>This is the post page!</h1>
+                    {/*aqui eu vou fazer uma requisição para o backend*/}
+                    <Button variant="filled" color="indigo">Get a web post</Button>
+                    {/*aqui eu vou fecho a requisição para o backend*/}
+                </div>
+        </MantineProvider>
     );
 };
 export default Posts;
