cd1a54d56fbec3f6982f8e2c72ea075f440d4491
Zewppie <abedwalid24@gmail.com>
2024-06-22 20:07:08 -0300

    Small fix for the menu bar in the entity page, it looked different from the other pages

--- frontend/src/pages/entities.tsx
+++ frontend/src/pages/entities.tsx
@@ -19,19 +19,21 @@ function Entities() {
             <div style={{ height: '70px' }} />
 
             {/* Fixed header with black background */}
-            <div className="header" style={{ position: 'fixed', top: 0, left: 0, right: 0, width: '100%', display: 'flex', alignItems: 'center', padding: '10px', backgroundColor: 'black', color: 'white', zIndex: 1000 }}>
+            <div className="header" style={{ position: 'fixed', top: 0, left: 0, right: 0, width: '100%', display: 'flex', alignItems: 'center', justifyContent: 'space-between', padding: '10px', backgroundColor: 'black', color: 'white', zIndex: 1000 }}>
                 <Link to="/">
                     <img src="the_old_world_archives_logo.png" alt="Logo" className="logo" style={{ marginRight: '20px' }} />
                 </Link>
-                <Link to="/user/register">
-                    <Button variant="filled" color="indigo" style={{ marginRight: '10px' }}>Register</Button>
-                </Link>
-                <Link to="/posts">
-                    <Button variant="filled" color="indigo" style={{ marginRight: '10px' }}>Posts</Button>
-                </Link>
-                <Link to="/entities">
-                    <Button variant="filled" color="indigo">Entity Wiki</Button>
-                </Link>
+                <div>
+                    <Link to="/user/register">
+                        <Button variant="filled" color="indigo" style={{ marginRight: '10px' }}>Register</Button>
+                    </Link>
+                    <Link to="/posts">
+                        <Button variant="filled" color="indigo" style={{ marginRight: '10px' }}>Posts</Button>
+                    </Link>
+                    <Link to="/entities">
+                        <Button variant="filled" color="indigo">Entity Wiki</Button>
+                    </Link>
+                </div>
             </div>
 
             {/* padding top serve pra barra de cima não ocultar os conteudos */}
